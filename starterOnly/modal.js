3a60b01452b33cdff2b2d77ce535511b95a2e0c7
Eniko GUTI <enikoguti@MacBook-Pro-de-Eniko.local>
2021-08-13 11:16:51 -0500

    JS close modal added
    JS function added

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,8 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeModalBtn = document.querySelector(".close");
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,5 +21,13 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+// close modal form
+closeModalBtn.addEventListener('click', function() {
+    modalbg.style.display = "none";
+  });
+
+
+
 
 
