10f05701e68a13e18d8ccea7448402309072e7d3
Eniko GUTI <enikoguti@MacBook-Pro-de-Eniko.local>
2021-08-25 12:17:02 -0500

    Css error messages and comments JS

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -27,6 +27,7 @@ closeModalBtn.addEventListener('click', function() {
     modalbg.style.display = "none";
   });
 
+
 // form validation 
 
 const form = document.getElementById("form");
@@ -55,43 +56,56 @@ form.addEventListener('submit', (e) => {
   
   let messages = []
 
-  console.log(checkbox1);
+  //check first name
 
   if (firstName.value === '' || firstName.value == null || firstName.value.length <= 2) {
     errorFirstNameElement.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
     messages.push("error");
   }
 
+  //check last name
+
   if (lastName.value === '' || lastName.value == null || lastName.value.length <= 2) {
     errorLastNameElement.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
     messages.push("error");
   }
+
+  //check email
 
   if(email.value === '' || email.value == null) {
     errorEmailElement.innerHTML = "Veuillez entrer un email valide.";
     messages.push("error");
   }
 
+  //check birthdate
+
   if(birthdate.value === '' || birthdate.value == null) {
     errorBirthDateElement.innerHTML = "Veuillez entrer votre date de naissance.";
     messages.push("error");
   }
+
+  //check participation to former competitions 
 
   if(quantity.value == '') {
     errorQuantityElement.innerHTML = "Veuillez indiquer le nombre de tournoi.";
     messages.push("error");
   }
 
+  //check location
+
   if(location1.checked == false && location2.checked == false && location3.checked == false && location4.checked == false && location5.checked == false && location6.checked == false) {
     errorLocationElement.innerHTML = "Veuillez cocher une ville.";
     messages.push("error");
   }
+
+  //check conditions accepation
 
   if(checkbox1.checked == false) {
     errorConditionElement.innerHTML = "Veuillez accepter les conditions d'utilisation.";
     messages.push("error");
   }
 
+  //Check error-messages and submit conditions
 
   if (messages.length == 0) {
     alert("Merci ! Votre réservation a été reçue.") 
